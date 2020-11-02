b07d311be6a3060a625f471c5e0be01c4acfe9f1
André Zimmermann <andrezimmermann@email.com>
2020-11-02 19:09:30 +0900

    Created the QrBrand for CPM and MPM and also the OnlineBran. (#87)
    Allow the payment data when POSTing to use the respective brand.
    When reading the transaction token creation response also correctly serialize the brand field.

--- src/main/java/com/univapay/sdk/models/response/transactiontoken/QrScanPaymentData.java
+++ src/main/java/com/univapay/sdk/models/response/transactiontoken/QrScanPaymentData.java
@@ -2,16 +2,17 @@ package com.univapay.sdk.models.response.transactiontoken;
 
 import com.google.gson.annotations.SerializedName;
 import com.univapay.sdk.types.Gateway;
-import com.univapay.sdk.types.QRBrand;
+import com.univapay.sdk.types.brand.QrCpmBrand;
 
 public class QrScanPaymentData {
+
   @SerializedName("gateway")
-  private Gateway gateway;
+  private final Gateway gateway;
 
   @SerializedName("brand")
-  private QRBrand brand;
+  private final QrCpmBrand brand;
 
-  public QrScanPaymentData(Gateway gateway, QRBrand brand) {
+  public QrScanPaymentData(Gateway gateway, QrCpmBrand brand) {
     this.gateway = gateway;
     this.brand = brand;
   }
@@ -20,7 +21,7 @@ public class QrScanPaymentData {
     return gateway;
   }
 
-  public QRBrand getBrand() {
+  public QrCpmBrand getBrand() {
     return brand;
   }
 }
