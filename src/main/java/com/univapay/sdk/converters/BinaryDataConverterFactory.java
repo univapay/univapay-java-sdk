630465e7ebecae2322dd3c3d0c64d931d8001b28
André Zimmermann <a.zimmermann@univapay.com>
2021-06-14 17:03:59 +0900

    Clean up

--- src/main/java/com/univapay/sdk/converters/BinaryDataConverterFactory.java
+++ src/main/java/com/univapay/sdk/converters/BinaryDataConverterFactory.java
@@ -1,7 +1,6 @@
 package com.univapay.sdk.converters;
 
 import com.univapay.sdk.models.response.UnivapayBinaryData;
-import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import javax.annotation.Nullable;
@@ -17,14 +16,8 @@ public class BinaryDataConverterFactory extends Converter.Factory {
       Type type, Annotation[] annotations, Retrofit retrofit) {
 
     if (type.equals(UnivapayBinaryData.class)) {
-
-      return new Converter<ResponseBody, UnivapayBinaryData>() {
-        @Override
-        public UnivapayBinaryData convert(ResponseBody responseBody) throws IOException {
-          return new UnivapayBinaryData(responseBody.bytes());
-        }
-      };
-
+      return (Converter<ResponseBody, UnivapayBinaryData>)
+          responseBody -> new UnivapayBinaryData(responseBody.bytes());
     } else return null;
   }
 }
