c2c88285677a85fcd868c90652bd8fd4351c4ce0
André Zimmermann <a.zimmermann@univapay.com>
2022-01-11 20:31:38 +0900

    Remove `it.ozimov:java7-hamcrest-matchers` depenency

--- src/test/java/com/univapay/sdk/utils/ExponentialBackoffTest.java
+++ src/test/java/com/univapay/sdk/utils/ExponentialBackoffTest.java
@@ -1,13 +1,12 @@
 package com.univapay.sdk.utils;
 
-import static it.ozimov.cirneco.hamcrest.java7.J7Matchers.betweenInclusive;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.Assert;
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ExponentialBackoffTest {
@@ -16,33 +15,33 @@ public class ExponentialBackoffTest {
 
     ExponentialBackoff backoff = new ExponentialBackoff(1_000, 20_000, 1.5, 0.5);
 
-    Assert.assertEquals(1000, backoff.currentIntervalMillis, 1);
+    assertEquals(1000, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(1500, backoff.currentIntervalMillis, 1);
+    assertEquals(1500, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(2250, backoff.currentIntervalMillis, 1);
+    assertEquals(2250, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(3375, backoff.currentIntervalMillis, 1);
+    assertEquals(3375, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(5062, backoff.currentIntervalMillis, 1);
+    assertEquals(5062, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(7593, backoff.currentIntervalMillis, 1);
+    assertEquals(7593, backoff.currentIntervalMillis, 1);
 
     backoff = new ExponentialBackoff(0, 30_000, 2, 0.5);
 
-    Assert.assertEquals(0, backoff.currentIntervalMillis, 0);
+    assertEquals(0, backoff.currentIntervalMillis, 0);
     backoff.calcNextInterval();
-    Assert.assertEquals(2000, backoff.currentIntervalMillis, 1);
+    assertEquals(2000, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(4000, backoff.currentIntervalMillis, 1);
+    assertEquals(4000, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(8000, backoff.currentIntervalMillis, 1);
+    assertEquals(8000, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(16000, backoff.currentIntervalMillis, 1);
+    assertEquals(16000, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(30000, backoff.currentIntervalMillis, 1);
+    assertEquals(30000, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(30000, backoff.currentIntervalMillis, 1);
+    assertEquals(30000, backoff.currentIntervalMillis, 1);
   }
 
   @Test
@@ -50,23 +49,23 @@ public class ExponentialBackoffTest {
 
     ExponentialBackoff backoff = new ExponentialBackoff(1_000, 20_000, 2, 0.5);
 
-    Assert.assertEquals(1000, backoff.currentIntervalMillis, 1);
+    assertEquals(1000, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(2000, backoff.currentIntervalMillis, 1);
+    assertEquals(2000, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(4000, backoff.currentIntervalMillis, 1);
+    assertEquals(4000, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(8000, backoff.currentIntervalMillis, 1);
+    assertEquals(8000, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(16000, backoff.currentIntervalMillis, 1);
+    assertEquals(16000, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(20000, backoff.currentIntervalMillis, 1);
+    assertEquals(20000, backoff.currentIntervalMillis, 1);
     backoff.calcNextInterval();
-    Assert.assertEquals(20000, backoff.currentIntervalMillis, 1);
+    assertEquals(20000, backoff.currentIntervalMillis, 1);
   }
 
   @Test
-  public void shouldMakeUniformlyDistributedRandomeBackoff() {
+  public void shouldMakeUniformlyDistributedRandomBackoff() {
 
     List<Long> vals = new ArrayList<>();
     long sum = 0;
@@ -96,11 +95,11 @@ public class ExponentialBackoffTest {
     for (int i = 0; i < 10; i++) {
       ExponentialBackoff backoff = new ExponentialBackoff(1_000, 20_000, 1.5, 0.5);
 
-      Assert.assertThat(backoff.next(), betweenInclusive((long) (1000 * 0.5), (long) (1000 * 1.5)));
-      Assert.assertThat(backoff.next(), betweenInclusive((long) (1500 * 0.5), (long) (1500 * 1.5)));
-      Assert.assertThat(backoff.next(), betweenInclusive((long) (2250 * 0.5), (long) (2250 * 1.5)));
-      Assert.assertThat(backoff.next(), betweenInclusive((long) (3375 * 0.5), (long) (3375 * 1.5)));
-      Assert.assertThat(backoff.next(), betweenInclusive((long) (5062 * 0.5), (long) (5062 * 1.5)));
+      assertThat(backoff.next(), betweenInclusive((long) (1000 * 0.5), (long) (1000 * 1.5)));
+      assertThat(backoff.next(), betweenInclusive((long) (1500 * 0.5), (long) (1500 * 1.5)));
+      assertThat(backoff.next(), betweenInclusive((long) (2250 * 0.5), (long) (2250 * 1.5)));
+      assertThat(backoff.next(), betweenInclusive((long) (3375 * 0.5), (long) (3375 * 1.5)));
+      assertThat(backoff.next(), betweenInclusive((long) (5062 * 0.5), (long) (5062 * 1.5)));
     }
   }
 
@@ -109,11 +108,16 @@ public class ExponentialBackoffTest {
 
     for (int i = 0; i < 10; i++) {
       ExponentialBackoff backoff = new ExponentialBackoff(1_000, 30_000, 2, 1.5);
-      Assert.assertThat(backoff.next(), greaterThanOrEqualTo((long) 0));
-      Assert.assertThat(backoff.next(), greaterThanOrEqualTo((long) 0));
-      Assert.assertThat(backoff.next(), greaterThanOrEqualTo((long) 0));
-      Assert.assertThat(backoff.next(), greaterThanOrEqualTo((long) 0));
-      Assert.assertThat(backoff.next(), greaterThanOrEqualTo((long) 0));
+      assertThat(backoff.next(), greaterThanOrEqualTo((long) 0));
+      assertThat(backoff.next(), greaterThanOrEqualTo((long) 0));
+      assertThat(backoff.next(), greaterThanOrEqualTo((long) 0));
+      assertThat(backoff.next(), greaterThanOrEqualTo((long) 0));
+      assertThat(backoff.next(), greaterThanOrEqualTo((long) 0));
     }
+  }
+
+  // Moving
+  private <T extends Comparable<T>> Matcher<T> betweenInclusive(T min, T max) {
+    return allOf(greaterThanOrEqualTo(min), lessThanOrEqualTo(max));
   }
 }
