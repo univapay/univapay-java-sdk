df3c4213662098287535570c760d334378b1cdce
André Zimmermann <a.zimmermann@univapay.com>
2022-09-01 10:51:46 +0900

    CallMethod - Added HTTP_GET_MOBILE

--- src/main/java/com/univapay/sdk/models/common/CallMethod.java
+++ src/main/java/com/univapay/sdk/models/common/CallMethod.java
@@ -5,10 +5,12 @@ import com.google.gson.annotations.SerializedName;
 /** Representation of the method that is used to consume the issuerToken */
 public enum CallMethod {
 
-  /** The issuer token is a HTTP endpoint, invoked with GET */
+  /** The issuer token is an HTTP endpoint, invoked with GET */
   @SerializedName("http_get")
   HTTP_GET,
-  /** The issuer token is a HTTP endpoint, invoked with POST */
+  @SerializedName("http_get_mobile")
+  HTTP_GET_MOBILE,
+  /** The issuer token is an HTTP endpoint, invoked with POST */
   @SerializedName("http_post")
   HTTP_POST,
   /** The issuer token should be forwarded to the service's provided SDK */
