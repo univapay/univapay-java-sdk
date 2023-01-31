3cf375f7c2492113675ac0e972b617687c8f53d9
André Zimmermann <a.zimmermann@univapay.com>
2023-01-31 16:39:19 +0900

    Card Brand: Private Label (#343)

--- src/main/java/com/univapay/sdk/types/CardBrand.java
+++ src/main/java/com/univapay/sdk/types/CardBrand.java
@@ -1,7 +1,10 @@
 package com.univapay.sdk.types;
 
 import com.google.gson.annotations.SerializedName;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public enum CardBrand {
   @SerializedName("visa")
@@ -9,7 +12,12 @@ public enum CardBrand {
   @SerializedName("mastercard")
   MASTERCARD("^(5[1-5]|222[1-9]|22[3-9]|2[3-6]|27[0-1]|2720)\\d*", 16, 16, 3, "mastercard"),
   @SerializedName("discover")
-  DISCOVER("^(6011|65|64[4-9]|622)\\d*", 16, 16, 3, "discover"),
+  DISCOVER(
+      "^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$",
+      16,
+      16,
+      3,
+      "discover"),
   @SerializedName("american_express")
   AMERICAN_EXPRESS("^3[47]\\d*", 15, 15, 4, "american_express"),
   @SerializedName("diners_club")
@@ -19,11 +27,14 @@ public enum CardBrand {
   @SerializedName("maestro")
   MAESTRO("^(5018|5020|5038|6020|6304|6703|6759|676[1-3])\\d*", 12, 19, 3, "maestro"),
   @SerializedName("unionpay")
-  UNIONPAY("^62\\d*", 16, 19, 3, "unionpay"),
+  UNIONPAY("^(62[0-9]{14,17})$", 16, 19, 3, "unionpay"),
+
+  @SerializedName("private_label")
+  PRIVATE_LABEL(12, 19, 3, "private_label"),
   @SerializedName("unknown")
   UNKNOWN("\\d+", 12, 19, 3, "unknown"),
   @SerializedName("empty")
-  EMPTY("^$", 12, 19, 3, "empty");
+  EMPTY("^$", 12, 19, 3, "empty", true);
 
   private static final int[] AMERICAN_EXPRESS_SPACE_INDICES = {4, 10};
   private static final int[] DEFAULT_SPACE_INDICES = {4, 8, 12};
@@ -34,18 +45,47 @@ public enum CardBrand {
   private final int securityCodeLength;
   private final String brandName;
 
+  private final boolean canMatch;
+
+  CardBrand(
+      String regex,
+      int minCardLength,
+      int maxCardLength,
+      int securityCodeLength,
+      String brandName,
+      boolean canMatch) {
+
+    if (regex != null) {
+      this.pattern = Pattern.compile(regex);
+    } else {
+      this.pattern = null;
+    }
+
+    this.minCardLength = minCardLength;
+    this.maxCardLength = maxCardLength;
+    this.securityCodeLength = securityCodeLength;
+    this.brandName = brandName;
+    this.canMatch = canMatch;
+  }
+
   CardBrand(
       String regex,
       int minCardLength,
       int maxCardLength,
       int securityCodeLength,
       String brandName) {
-    this.pattern = Pattern.compile(regex);
-    this.minCardLength = minCardLength;
-    this.maxCardLength = maxCardLength;
-    this.securityCodeLength = securityCodeLength;
-    this.brandName = brandName;
+    this(regex, minCardLength, maxCardLength, securityCodeLength, brandName, true);
   }
+
+  CardBrand(int minCardLength, int maxCardLength, int securityCodeLength, String brandName) {
+    this(null, minCardLength, maxCardLength, securityCodeLength, brandName, false);
+  }
+
+  // Used at forCardNumber
+  private static final Collection<CardBrand> possibleToMatch =
+      Arrays.stream(CardBrand.values())
+          .filter(value -> value.canMatch)
+          .collect(Collectors.toList());
 
   /**
    * Returns the card type matching this account, or UNKNOWN for no match.
@@ -54,12 +94,10 @@ public enum CardBrand {
    * match.
    */
   public static CardBrand forCardNumber(String cardNumber) {
-    for (CardBrand cardType : values()) {
-      if (cardType.getPattern().matcher(cardNumber).matches()) {
-        return cardType;
-      }
-    }
-    return EMPTY;
+    return possibleToMatch.stream()
+        .filter(brand -> brand.getPattern().matcher(cardNumber).matches())
+        .findFirst()
+        .orElse(EMPTY);
   }
 
   public static CardBrand forBrandName(String brandName) {
@@ -142,7 +180,7 @@ public enum CardBrand {
     final int numberLength = cardNumber.length();
     if (numberLength < minCardLength || numberLength > maxCardLength) {
       return false;
-    } else if (!pattern.matcher(cardNumber).matches()) {
+    } else if (pattern != null && !pattern.matcher(cardNumber).matches()) {
       return false;
     }
     return isLuhnValid(cardNumber);
