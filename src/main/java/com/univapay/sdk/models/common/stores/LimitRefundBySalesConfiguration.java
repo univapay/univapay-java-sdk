889cedfc38deca44c85294bfc7b421099658b8b2
Atsushi Asai <a.asai@univapay.com>
2024-08-01 18:26:20 +0900

    Added builder

--- src/main/java/com/univapay/sdk/models/common/stores/LimitRefundBySalesConfiguration.java
+++ src/main/java/com/univapay/sdk/models/common/stores/LimitRefundBySalesConfiguration.java
@@ -3,12 +3,14 @@ package com.univapay.sdk.models.common.stores;
 import com.google.gson.annotations.SerializedName;
 import com.univapay.sdk.types.SubscriptionPeriod;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class LimitRefundBySalesConfiguration {
     @SerializedName("enabled")
     private Boolean enabled;
