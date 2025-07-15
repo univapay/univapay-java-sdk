4359137002827e9d2482bf158f6132d5b2d80f2c
André Zimmermann <a.zimmermann@univapay.com>
2025-07-15 16:17:39 +0900

    Add new RefundReasons (#503)

--- src/main/java/com/univapay/sdk/types/RefundReason.java
+++ src/main/java/com/univapay/sdk/types/RefundReason.java
@@ -11,6 +11,10 @@ public enum RefundReason {
   CUSTOMER_REQUEST,
   @SerializedName("chargeback")
   CHARGEBACK,
+  @SerializedName("chargeback_fee_exempt")
+  CHARGEBACK_FEE_EXEMPT,
+  @SerializedName("chargeback_reverse")
+  CHARGEBACK_REVERSE,
   @SerializedName("system_failure")
   SYSTEM_FAILURE
 }
